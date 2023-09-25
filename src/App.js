67316e27abd48c69db9bf8d3380575feaa0fba07
Chiou Jia Sheng <100426464+Chious@users.noreply.github.com>
2023-09-25 14:39:19 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import MainGame from "./component/MainGame/MainGame";
-import Menu from "./component/Menu";
-import Rule from "./component/Rule";
-import Starter from "./component/Starter";
 import "./style/App.scss";
 
 function App() {
